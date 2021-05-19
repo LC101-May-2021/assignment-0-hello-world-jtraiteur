f2d4c4279e00387f3b49443f655d1d0ddbadd89a
jtraiteur <>
2021-05-19 03:51:14 +0000

    added World

--- hello.js
+++ hello.js
@@ -1,7 +1,5 @@
-function hello() {
-	// TODO: change this string so that your program prints "Hello world!" 
-	// when you hit the replit run button
-	return "Hello World!";
-}
+
  
-module.exports = hello;
+ 
+console.log("Hello World!");
+
